f09b9f780bd842ab760d051cd962940fa1bb59de
Dima Shirkov <dimashirkov@list.ru>
2023-01-04 18:05:05 +0300

    removed the hitman variable in the getting JSON object test

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -11,12 +11,5 @@ test('getting JSON object', async () => {
       points: 2000,
     },
   };
-
-  const hitman = await GameSavingLoader.load();
-  expect(JSON.parse(hitman)).toEqual(hitmanParameters);
+  expect(await GameSavingLoader.load()).toEqual(hitmanParameters);
 });
-
-// test('getting an error', async () => {
-//     const hitman = await GameSavingLoader.load();
-//     expect(JSON.parse(hitman)).toEqual(hitmanParameters);
-// });
